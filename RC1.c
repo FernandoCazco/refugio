cf9b3e011392dd6c0260f6249e49b11ee8ac64e7
FernandoCazco <fernando.cazco.guazhima@udla.edu.ec>
2026-05-17 22:21:58 -0500

    funcion menu() y lectura de opcion
    Se implementa menu() con las 6 opciones del sistema y se conecta al ciclo do-while en main()

--- RC1.c
+++ RC1.c
@@ -3,6 +3,8 @@
 
 #define MAX 5
 
+void menu();
+
 int main(){
     char especies[MAX][50];
     float pesos[MAX];
@@ -20,8 +22,19 @@ int main(){
     scanf("%d", &personal);
 
     do {
+        menu();
         scanf("%d", &op);
     } while(op != 6);
 
     return 0;
+}
+
+void menu(){
+    printf("\n1- registrar animal\n");
+    printf("2- editar animal\n");
+    printf("3- dar de alta (liberar)\n");
+    printf("4- ver animales\n");
+    printf("5- evaluar capacidad del refugio\n");
+    printf("6- salir\n");
+    printf("opcion: ");
 }
